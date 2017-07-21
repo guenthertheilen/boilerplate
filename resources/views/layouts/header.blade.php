6a0b4c1ad5c3a670001aea39d1f6066b5ccc081a
Guenther Theilen <mail@guenthertheilen.de>
2017-07-21 21:07:46 +0200

    basic header layout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,17 +1,28 @@
-<a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
-<ul>
-    @if (Auth::guest())
-        <li><a href="{{ route('login') }}">Login</a></li>
-        <li><a href="{{ route('register') }}">Register</a></li>
-    @else
-        <li><a href="#">{{ Auth::user()->name }}</a></li>
-        <li><a href="{{ route('logout') }}"
-               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                Logout
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-            </form>
-        </li>
-    @endif
-</ul>
+<nav class="navbar">
+    <div class="navbar-brand">
+        <a href="{{ url('/') }}" class="navbar-item">{{ config('app.name', 'Laravel') }}</a>
+        <div class="navbar-burger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <div class="navbar-menu">
+        <div class="navbar-end">
+            @if (Auth::guest())
+                <a href="{{ route('login') }}" class="navbar-item">Login</a>
+                <a href="{{ route('register') }}" class="navbar-item">Register</a>
+            @else
+                <a href="#" class="navbar-item">{{ Auth::user()->name }}</a>
+                <a href="{{ route('logout') }}"
+                   class="navbar-item"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            @endif
+        </div>
+    </div>
+</nav>
