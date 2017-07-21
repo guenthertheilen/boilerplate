4477bc0a73778d05674904935725e72ec5633943
Guenther Theilen <mail@guenthertheilen.de>
2017-07-21 19:40:15 +0200

    Basic layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,24 +9,28 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
-<ul>
-    @if (Auth::guest())
-        <li><a href="{{ route('login') }}">Login</a></li>
-        <li><a href="{{ route('register') }}">Register</a></li>
-    @else
-        <li><a href="#">{{ Auth::user()->name }}</a></li>
-        <li><a href="{{ route('logout') }}"
-               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                Logout
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-            </form>
-        </li>
-    @endif
-</ul>
-@yield('content')
+<div class="columns">
+    <div class="column is-8 is-offset-2">
+        <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+        <ul>
+            @if (Auth::guest())
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+            @else
+                <li><a href="#">{{ Auth::user()->name }}</a></li>
+                <li><a href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+            @endif
+        </ul>
+        @yield('content')
+    </div>
+</div>
 <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
