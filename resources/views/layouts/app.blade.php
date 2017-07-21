83916f5ea4e83dd0c18a99cee4af479975383535
Guenther Theilen <mail@guenthertheilen.de>
2017-07-21 20:02:37 +0200

    Extract header into partial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,23 +11,7 @@
 <body>
 <div class="columns">
     <div class="column is-8 is-offset-2">
-        <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
-        <ul>
-            @if (Auth::guest())
-                <li><a href="{{ route('login') }}">Login</a></li>
-                <li><a href="{{ route('register') }}">Register</a></li>
-            @else
-                <li><a href="#">{{ Auth::user()->name }}</a></li>
-                <li><a href="{{ route('logout') }}"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
-            @endif
-        </ul>
+        @include("layouts.header")
         @yield('content')
     </div>
 </div>
