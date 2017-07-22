b011e97d512fb8a72841f1384c014bb68e5b43fa
Guenther Theilen <mail@guenthertheilen.de>
2017-07-22 22:46:26 +0200

    Test access control

--- tests/Browser/access_control/LoginTest.php
+++ tests/Browser/access_control/LoginTest.php
@@ -1,21 +1,60 @@
 <?php
 
-namespace Tests\Browser\access_control;
+namespace Tests\Browser\AccessControl;
 
+use App\User;
 use Auth;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class LoginTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+        Auth::logout();
+        factory(User::class)->create([
+            'email' => 'abc@example.com',
+            'password' => bcrypt('123456')
+        ]);
+    }
+
     /** @test */
     function it_shows_login_page_if_guest_tries_to_access_homepage()
     {
-        Auth::logout();
-
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                 ->assertPathIs(route('login', [], false));
         });
     }
+
+//    /** @test */
+//    function it_logs_in_visitor_with_valid_credentials()
+//    {
+//        $this->browse(function (Browser $browser) {
+//            $browser->visit('/')
+//                ->type('email', 'abc@example.com')
+//                ->type('password', '123456')
+//                ->press(__('Login'))
+//                ->assertPathIs('/');
+//        });
+//    }
+//
+//    /** @test */
+//    function it_does_not_log_in_visitor_with_invalid_credentials()
+//    {
+//        $this->browse(function (Browser $browser) {
+//            $browser->visit('/')
+//                ->type('email', 'INVALID@example.com')
+//                ->type('password', '123456')
+//                ->press(__('Login'))
+//                ->type('email', 'abc@example.com')
+//                ->type('password', 'INVALID')
+//                ->press(__('Login'))
+//                ->assertPathIs(route('login', [], false));
+//        });
+//    }
 }
--- tests/Browser/access_control/RegistrationTest.php
+++ tests/Browser/access_control/RegistrationTest.php
@@ -1,15 +1,18 @@
 <?php
 
-namespace Tests\Browser;
+namespace Tests\Browser\AccessControl;
 
+use Auth;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class RegistrationTest extends DuskTestCase
 {
     /** @test */
-    function it_shows_registration_link()
+    function it_shows_registration_link_for_guest()
     {
+        Auth::logout();
+
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                 ->clickLink(__('Register'))
