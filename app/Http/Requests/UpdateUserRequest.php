8e4871b9cf7ff15520b86079f0c3e1659fd5f3b6
Guenther Theilen <mail@guenthertheilen.de>
2017-08-03 21:28:03 +0200

    User update requires email

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -24,7 +24,8 @@ class UpdateUserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required'
+            'name' => 'required',
+            'email' => 'required'
         ];
     }
 }
--- tests/Feature/AccessControl/UsersTest.php
+++ tests/Feature/AccessControl/UsersTest.php
@@ -47,19 +47,52 @@ class UsersTest extends TestCase
     }
 
     /** @test */
-    function it_updates_user()
+    function it_updates_users_name()
     {
-        $user = factory(User::class)->create(['name' => 'foo', 'email' => 'foo@example.com']);
+        $user = factory(User::class)->create(['name' => 'foo']);
+        $role = factory(Role::class)->create();
+        $user->attachRole($role);
+
+        $this->patch(route('user.update', $user->id), ['name' => 'bar', 'email' => $user->email, 'roles' => [$role->id]])
+            ->assertRedirect(route('user.index'));
+
+        $this->assertDatabaseHas('users', ['name' => 'bar', 'email' => $user->email]);
+        $this->assertDatabaseMissing('users', ['name' => 'foo']);
+
+        $user->refresh();
+        $this->assertTrue($user->hasRole($role));
+    }
+
+    /** @test */
+    function it_updates_users_email()
+    {
+        $user = factory(User::class)->create(['email' => 'foo@example.com']);
+        $role = factory(Role::class)->create();
+        $user->attachRole($role);
+
+        $this->patch(route('user.update', $user->id), ['name' => $user->name, 'email' => 'bar@example.com', 'roles' => [$role->id]])
+            ->assertRedirect(route('user.index'));
+
+        $this->assertDatabaseHas('users', ['name' => $user->name, 'email' => 'bar@example.com']);
+        $this->assertDatabaseMissing('users', ['email' => 'foo@example.com']);
+
+        $user->refresh();
+        $this->assertTrue($user->hasRole($role));
+    }
+
+    /** @test */
+    function it_updates_users_roles()
+    {
+        $user = factory(User::class)->create();
         $role1 = factory(Role::class)->create();
         $role2 = factory(Role::class)->create();
         $user->attachRole($role1);
 
-        $this->patch(route('user.update', $user->id), ['name' => 'bar', 'email' => 'bar@example.com', 'roles' => [$role2->id]])
+        $this->patch(route('user.update', $user->id), ['name' => $user->name, 'email' => $user->email, 'roles' => [$role2->id]])
             ->assertRedirect(route('user.index'));
 
-        $this->assertDatabaseHas('users', ['name' => 'bar', 'email' => 'bar@example.com']);
-        $this->assertDatabaseMissing('users', ['name' => 'foo']);
-        $this->assertDatabaseMissing('users', ['email' => 'foo@example.com']);
+        $this->assertDatabaseHas('users', ['name' => $user->name, 'email' => $user->email]);
+        $this->assertCount(1, User::all());
 
         $user->refresh();
         $this->assertFalse($user->hasRole($role1));
@@ -76,5 +109,17 @@ class UsersTest extends TestCase
         $this->patch(route('user.update', $user->id), ['name' => '', 'email' => $user->email, 'roles' => [$role->id]]);
 
         $this->assertDatabaseMissing('users', ['name' => '']);
+    }
+
+    /** @test */
+    function it_does_not_update_user_without_email()
+    {
+        $user = factory(User::class)->create(['email' => 'foo@bar.com']);
+        $role = factory(Role::class)->create();
+        $user->attachRole($role);
+
+        $this->patch(route('user.update', $user->id), ['name' => $user->name, 'email' => '', 'roles' => [$role->id]]);
+
+        $this->assertDatabaseMissing('users', ['email' => '']);
     }
 }
