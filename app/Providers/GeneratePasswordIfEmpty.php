f73e3ebbc646f950e65e61c99164dadbd0be1c49
Guenther Theilen <mail@guenthertheilen.de>
2017-08-06 22:11:02 +0200

    Move password generation and encryption to service class for easier testing.

--- app/Providers/GeneratePasswordIfEmpty.php
+++ app/Providers/GeneratePasswordIfEmpty.php
@@ -3,16 +3,23 @@
 namespace App\Providers;
 
 use App\Events\UserCreated;
+use App\Services\Password;
 
 class GeneratePasswordIfEmpty
 {
     /**
+     * @var Password
+     */
+    private $password;
+
+    /**
      * Create the event listener.
      *
+     * @param Password $password
      */
-    public function __construct()
+    public function __construct(Password $password)
     {
-        //
+        $this->password = $password;
     }
 
     /**
@@ -24,7 +31,8 @@ class GeneratePasswordIfEmpty
     public function handle(UserCreated $event)
     {
         if (empty($event->user->getAttribute('password'))) {
-            $event->user->update(['password' => bcrypt(str_random())]);
+            $password = $this->password->generate();
+            $event->user->update(['password' => $this->password->encrypt($password)]);
         }
     }
 }
--- tests/Unit/Listeners/GeneratePasswordIfEmptyTest.php
+++ tests/Unit/Listeners/GeneratePasswordIfEmptyTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Listeners;
 use App\Events\UserCreated;
 use App\Models\User;
 use App\Providers\GeneratePasswordIfEmpty;
+use App\Services\Password;
 use Mockery;
 use Tests\TestCase;
 
@@ -14,11 +15,14 @@ class GeneratePasswordIfEmptyTest extends TestCase
     function it_generates_password_if_empty()
     {
         $user = Mockery::mock(User::class);
+        $password = Mockery::mock(Password::class);
         $user->shouldReceive('getAttribute')->with('password')->once()->andReturn('');
+        $password->shouldReceive('generate')->once()->andReturn('some-password');
+        $password->shouldReceive('encrypt')->with('some-password')->once()->andReturn('encrypted-password');
 
-        $user->shouldReceive('update')->once();
+        $user->shouldReceive('update')->with(['password' => 'encrypted-password'])->once();
 
-        $listener = new GeneratePasswordIfEmpty();
+        $listener = new GeneratePasswordIfEmpty($password);
         $listener->handle(new UserCreated($user));
     }
 
@@ -26,10 +30,12 @@ class GeneratePasswordIfEmptyTest extends TestCase
     function it_does_not_change_password_if_it_is_already_set()
     {
         $user = Mockery::mock(User::class);
+        $password = Mockery::mock(Password::class);
         $user->shouldReceive('getAttribute')->with('password')->once()->andReturn('foobar');
         $user->shouldNotReceive('update');
+        $password->shouldNotReceive('generate');
 
-        $listener = new GeneratePasswordIfEmpty();
+        $listener = new GeneratePasswordIfEmpty($password);
         $listener->handle(new UserCreated($user));
     }
 }
