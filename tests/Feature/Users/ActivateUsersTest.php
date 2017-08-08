5559d62e48ae16f33a54552ff9eeda85893433fc
Guenther Theilen <mail@guenthertheilen.de>
2017-08-08 22:37:27 +0200

    Activate user (wip)

---
+++ tests/Feature/Users/ActivateUsersTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Users;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ActivateUsersTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function it_activates_a_user()
+    {
+
+    }
+}
