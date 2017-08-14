14a16de69f44b43d951066bb43d6a7eaf2d22378
Guenther Theilen <mail@guenthertheilen.de>
2017-08-14 20:19:04 +0200

    Remove IDE helper. vim for the win.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->environment() !== 'production') {
-            $this->app->register(IdeHelperServiceProvider::class);
             $this->app->register(DuskServiceProvider::class);
         }
     }
