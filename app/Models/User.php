26d09272e551a4d0e4c9242a7355873a6fca7e11
Guenther Theilen <mail@guenthertheilen.de>
2017-08-19 23:58:11 +0200

    fix PSR2 issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -104,7 +104,7 @@ class User extends Authenticatable
      */
     public function hasNoPassword()
     {
-	    return $this->password == '';
+        return $this->password == '';
     }
 
     /**
