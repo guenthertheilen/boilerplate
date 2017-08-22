e1ae071209e80369110672fdff2ce84f538de77e
Guenther Theilen <mail@guenthertheilen.de>
2017-08-22 19:25:09 +0200

    clean up code

--- app/Mail/ActivateAccount.php
+++ app/Mail/ActivateAccount.php
@@ -4,7 +4,6 @@ namespace App\Mail;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
@@ -14,22 +13,12 @@ class ActivateAccount extends Mailable
 
     public $user;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
     public function __construct(User $user)
     {
         $this->user = $user;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
+    public function build(): ActivateAccount
     {
         return $this->text('mail.activate_account');
     }
