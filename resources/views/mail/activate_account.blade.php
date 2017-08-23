d87ab39fe71921686426af2669f67b9ba2930b58
Guenther Theilen <mail@guenthertheilen.de>
2017-08-23 08:48:12 +0200

    activation mail

--- resources/views/mail/activate_account.blade.php
+++ resources/views/mail/activate_account.blade.php
@@ -1,3 +1,10 @@
 Hallo {{ $user->name }},
 
 Willkommen bei {{ config('app.name') }}.
+
+Um Ihrer Anmeldung abzuschließen, klicken Sie bitte auf
+{{ route('user.activate', $user-$token) }}
+@if($user->hasNoPassword())
+    und erstellen ein neues Passwort für Ihren Zugang.
+@endif
+
