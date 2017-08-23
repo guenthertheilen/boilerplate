8846503c1be1b40ce22c089e9c9a0998e20165d4
Guenther Theilen <mail@guenthertheilen.de>
2017-08-23 22:50:21 +0200

    fix typo

--- resources/views/mail/activate_account.blade.php
+++ resources/views/mail/activate_account.blade.php
@@ -2,7 +2,6 @@ Hallo {{ $user->name }},
 
 Willkommen bei {{ config('app.name') }}.
 
-Um Ihrer Anmeldung abzuschließen, klicken Sie bitte auf
+Um Ihre Anmeldung abzuschließen, klicken Sie bitte auf
 {{ route('user.activate', $user->activation_token) }}
 @if($user->hasNoPassword())und erstellen ein neues Passwort für Ihren Zugang.@endif
-
