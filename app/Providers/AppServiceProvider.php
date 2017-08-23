22a562344ccaf60b58cf44b4611237490f3daa49
Guenther Theilen <mail@guenthertheilen.de>
2017-08-23 11:03:50 +0200

    Laravel IDE helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Dusk\DuskServiceProvider;
@@ -28,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment() !== 'production') {
             $this->app->register(DuskServiceProvider::class);
+            $this->app->register(IdeHelperServiceProvider::class);
         }
     }
 }
