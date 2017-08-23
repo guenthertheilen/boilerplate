20108c9dd5c072d9d0b7ade94272a954527fa15f
Guenther Theilen <mail@guenthertheilen.de>
2017-08-23 21:58:57 +0200

    reformat html

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -1,30 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="title">@lang('Permissions')</h1>
+<h1 class="title">@lang('Permissions')</h1>
 
-    <table class="table">
-        <thead>
+<table class="table">
+    <thead>
         <tr>
             <th>@lang('Permission')</th>
             <th>@lang('Roles')</th>
             <th></th>
         </tr>
-        </thead>
-        <tbody>
+    </thead>
+    <tbody>
         @foreach($permissions as $permission)
-            <tr>
-                <td>{{ $permission->name }}</td>
-                <td>{{ $permission->rolesAsString() }}</td>
-                <td>
-                    <a href="{{ route('permission.edit', $permission->id) }}">
-                            <span class="icon is-small">
-                                <i class="fa fa-pencil"></i>
-                            </span>
-                    </a>
-                </td>
-            </tr>
+        <tr>
+            <td>{{ $permission->name }}</td>
+            <td>{{ $permission->rolesAsString() }}</td>
+            <td>
+                <a href="{{ route('permission.edit', $permission->id) }}">
+                    <span class="icon is-small">
+                        <i class="fa fa-pencil"></i>
+                    </span>
+                </a>
+            </td>
+        </tr>
         @endforeach
-        </tbody>
-    </table>
+    </tbody>
+</table>
 @endsection
